cc3c892435ef38e57352ea64f32f1970e8cbe4c1
Yeri Kwon <yeri_i@naver.com>
2015-09-29 22:16:06 -0700

    really recently code

--- apps/player.c
+++ apps/player.c
@@ -5,7 +5,6 @@ void main(void)
 	foo2();
 	printf("Kwon~~~~\n");
 	printf("debugging again\n");
-	printf("branch test\n");
-	printf("today`s lecture subject is branch and merge");
+	printf("github test\n");
 	foo1();
 }
